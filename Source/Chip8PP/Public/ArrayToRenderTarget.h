bf4126ead24ba2142022c2a633d1d9895ef29887
Niluji <julianmaciasavila95@gmail.com>
2025-02-06 00:58:09 +0100

    Fix material setup

--- Source/Chip8PP/Public/ArrayToRenderTarget.h
+++ Source/Chip8PP/Public/ArrayToRenderTarget.h
@@ -13,15 +13,4 @@ UCLASS()
 class CHIP8PP_API UArrayToRenderTarget : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
-public:
-    /**
-    * Draws a boolean array onto a render target as a grayscale texture.
-    * @param RenderTarget - The render target to draw onto.
-    * @param BoolArray - The array of boolean values representing pixel states.
-    * @param Width - The width of the render target.
-    * @param Height - The height of the render target.
-    */
-	UFUNCTION(BlueprintCallable, Category = "Rendering")
-	static void DrawBoolArrayToRenderTarget(UTextureRenderTarget2D* RenderTarget, const TArray<bool>& BoolArray, int32 Width, int32 Height);
 };
